8aad877149239b1d2c960809e1dcb7311ae3f2f1
Danial Hanif <danialf205t@gmail.com>
2019-10-28 03:04:26 +0800

    Changes to User class and prepared Management class
    Added Company, Job, Service, Hotel and UserList struct.
    Changed User class skill vector type variable to struct type and added UserData struct.
    Changed addSkills and deleteSkills function of User class
    Prepared Management header for future implementation.

---
+++ src/Management.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "User.h"
+#include "Nodes.h"
+
+#ifndef MANAGEMENT_H
+#define MANAGEMENT_H
+
+class Management {
+
+	private:
+		User userData;
+		Company companyLists;
+		Job jobLists;
+		Service serviceLists;
+		Hotel hotelLists;
+
+
+
+};
+
+#endif
--- src/User.h
+++ src/User.h
@@ -7,16 +7,42 @@
 
 class User {
 
+	//skill with descriptions
+	struct Skills {
+
+		int id;
+		std::string name;
+		std::string description;
+		int level;
+		Skills* next;
+
+	};
+
+	//userdata structure
+	struct UserData {
+
+		int id;
+		std::string username;
+		std::string fullName;
+		std::string emailAddress;
+		std::string dateOfBirth;
+		std::string streetAddress;
+		std::string password;
+		std::string	registrationDate;
+		Skills* skills;
+	};
+
+
 
     protected:
-        int id;
-        std::string username, fullName, emailAddress, dateOfBirth, streetAddress, password, registrationDate;
-        std::vector<std::string> skills;
+
+		UserData* userData;
 
     public:
 
         User();
 
+		int getID();
         std::string getUsername();
         std::string getFullName();
         std::string getEmailAddress();
@@ -24,9 +50,10 @@ class User {
         std::string getStreetAddress();
         std::string getPassword();
         std::string getRegistrationDate();
-        std::vector<std::string> getSkills();
+        UserData getSkills();
 
         void setup();
+		void setID(int id);
         void setUsername(std::string);
         void setFullName(std::string);
         void setEmailAddress(std::string);
